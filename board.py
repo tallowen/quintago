d80690ae6396a8fa616681d7165b1b22a5553fd7
Owen Coutts <owen@owencoutts.com>
2011-08-31 17:17:01 -0700

    Rewrite in numpy

--- board.py
+++ board.py
@@ -1,45 +1,50 @@
+from numpy import array
+from numpy import zeros
+from numpy import rot90
 class quintago:
-    board = []
+    board = array(36*(0,))
     def __init__(self):
-        for i in range(6):
-            self.board.append([0,0,0,0,0,0])
-            
-    def place(self,x,y,color):       
-        self.board[x][y] = 1 if color else -1
-        
-    def get_quadrant(self, number):
-        if number not in [-1,0,1]:
-            raise Exception("You can only have a value of -1, 0, 1 as the value")
-            
-        quadrant = []
-        for i in (range(3) if number in [1,3] else range(3,6)):
-            quadrant.append(self.board[i][0:3] if number in [1,2] else self.board[i][3:6])
-            
-        return quadrant
-        
-    def rotate(self, quadrant_number,clockwise):
-        old = self.get_quadrant(quadrant_number)
-        new = [[],[],[]]
-        for x in range(3):
-            for y in range(3):
-                new[x].append(old[y][2-x])
+    	self.board.shape = (6,6)
                 
-        return new
+    def place(self,y,x,color):       
+        if x not in range(0,6):
+            raise Exception("You can only have an x value within 0-5")
+        if y not in range(0,6):
+            raise Exception("You can only have an y value within 0-5")
+        if color not in [True,False]:
+            raise Exception("You can only have a color with True,False as a valuehin 0-5")
+        self.board[y,x] = 1 if color else -1
+     
+    def rotate(self,quadrant_number,clockwise):
+    	r = 3 if clockwise else 1
+    	if quadrant_number ==1:
+    		self.board[0:3,0:3] = rot90(self.board[0:3,0:3].copy(),r)
+    	elif quadrant_number == 2:
+    		self.board[0:3,3:6] = rot90(self.board[0:3,3:6].copy(),r)
+    	elif quadrant_number == 3:
+    		self.board[3:6,0:3] = rot90(self.board[3:6,0:3].copy(),r)
+    	else:
+    		self.board[3:6,3:6] = rot90(self.board[3:6,3:6].copy(),r)
+    		     
+    def __repr__(self):
+    	return self.board.__repr__()
+
     
+"""
+    def get_quadrant(self, number):
+        if number not in [1,2,3,4]:
+            raise Exception("You can only choose a quadrant with [1,2,3,4] as value")
         
-    def to_console(self):
-        row_number = 0
-        for y in range(6):
-            row = ''
-            for x in range(6):
-                row+=str(self.board[x][y])+' '
-                if x ==2:
-                    row +="| "
-                
-            print row
-            if y == 2:
-                print '------x-------'
-        
+        #quadrant = zeros(shape(3,3),dtype='int')
+        if number == 1:
+        	return self.board[0:3,0:3]
+    	elif number == 2:
+    		return self.board[0:3,3:6]
+    	elif number == 3:
+    		return self.board[3:6,0:3]
+    	else:
+    		return self.board[3:6,3:6]
+    		"""
         
         
         
